8b716b339000af71f62e226ee320da142a6a5540
ARUNPRAVEENP <124482754+ARUNPRAVEENP@users.noreply.github.com>
2024-02-04 15:24:45 +0530

    Create ternaryoperator.java

---
+++ ternaryoperator.java
@@ -0,0 +1,15 @@
+
+public class Training {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		 int a = 20;
+		 //Ternary operator
+		 int rem = a%2;
+		 System.out.println((rem==0)?"Even":"odd");
+		 
+	
+	}
+
+}
